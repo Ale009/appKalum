78268b7953432670f73e552f6642daa0afddf206
“asalazar” <salazarale9@hotmail.com>
2021-10-09 13:27:21 -0600

    Eliminando archivos

--- src/index-importaciones.js
+++
@@ -1,8 +0,0 @@
-import cursosKalum, {instructoresKalum} from './data/cursos';
-
-const buscarCursosPorId = (id) => cursosKalum.find((elemento) => elemento.id === id);
-
-const buscarInstructorPorId = (id) => instructoresKalum.find((elemento) => elemento.id === id);
-console.log(buscarCursosPorId(4));
-console.log(buscarInstructorPorId(3));
-
