067e31cf263a3dc0446973f9250b8bd14d7fb369
“asalazar” <salazarale9@hotmail.com>
2021-11-06 16:45:59 -0600

    Agregando crear nuevo usuario y acceso del login

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Navigation from './layouts/Navigation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CarrerasTecnicas from './pages/CarrerasTecnicas';
+import Login from './pages/Login';
+import store from './store';
+import { Provider } from 'react-redux';
+import checkForToken from './utils/checkForToken';
+import PrivateRoute from './utils/PrivateRoute';
+import RegisterUser from './pages/RegisterUser'
+
+
+checkForToken();
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <div>
+                    <Navigation></Navigation>
+                </div>
+                <Container>
+                    <Switch>
+                        <PrivateRoute exact path="/carreras-tecnicas" component={CarrerasTecnicas}></PrivateRoute>
+                        <Route exact path="/login" component={Login}></Route>
+                        <Route exact path="/register-user" component = {RegisterUser}></Route>
+                    </Switch>
+                </Container>
+            </Router>
+        </Provider>
+    )
+}
+
+export default App;
---
+++ src/utils/endPoints.js
@@ -0,0 +1,6 @@
+const API_URL = 'http://localhost:9002';
+
+export const APP_USERNAME = 'kalumapp';
+export const APP_PASSWORD = 'Acce$o-7815';
+export const LOGIN_ENDPOINT = `${API_URL}/oauth/token`;
+export const REGISTER_USER_ENDPOINT = `${API_URL}/kalum-notas/v1/usuarios`
